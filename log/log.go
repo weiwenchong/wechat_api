cfff43db71129aebd0a7104e62994dc1e8440e95
weiwenchong <weiwenchong@bytedance.com>
2022-12-04 15:16:37 +0800

    1

--- log/log.go
+++ log/log.go
@@ -1,11 +1,8 @@
 package log
 
 import (
-	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/sirupsen/logrus"
-	"os"
-	"time"
 )
 
 var log = logrus.New()
@@ -13,15 +10,15 @@ var log = logrus.New()
 func init() {
 	log.Formatter = &logrus.JSONFormatter{}
 
-	now := time.Now().Format("2006.01.02.15:04:005")
-	fileName := fmt.Sprintf("gin_%s.log", now)
-	file, err := os.OpenFile(fileName, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
-	if err != nil {
-		fmt.Printf("create log file error:%v", err)
-		panic(err)
-	}
-
-	log.Out = file
+	//now := time.Now().Format("2006.01.02.15:04:005")
+	//fileName := fmt.Sprintf("gin_%s.log", now)
+	//file, err := os.OpenFile(fileName, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+	//if err != nil {
+	//	fmt.Printf("create log file error:%v", err)
+	//	panic(err)
+	//}
+	//
+	//log.Out = file
 
 	gin.SetMode(gin.ReleaseMode)
 	gin.DefaultWriter = log.Out
